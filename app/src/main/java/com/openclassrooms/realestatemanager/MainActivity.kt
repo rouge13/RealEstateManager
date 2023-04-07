944e399f1c5b6dcd39f9a31295577f39f41e2f87
rouge13 <62237564+rouge13@users.noreply.github.com>
2023-04-07 20:11:25 +0200

    MainActivityLayout, FragmentListLayout, menuLayout and all the pictures and icons required for this project. And just MainActivity with the PropertyListFragment to show if it's running in UI.

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.openclassrooms.realestatemanager
+
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.realestatemanager.R
+import com.example.realestatemanager.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import com.openclassrooms.realestatemanager.property_list.PropertyListFragment
+
+/**
+ * Created by Julien HAMMER - Apprenti Java with openclassrooms on .
+ */
+class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+        drawerLayout = activityMainBinding.activityMainDrawerLayout
+        navigationView = activityMainBinding.activityMainNavView
+
+        // Set up the drawer toggle
+        setupDrawerButton()
+
+        // Set up the navigation view from the drawerLayout
+        setupDrawerContent(navigationView)
+
+        // Loat the initial fragment into the fragment container
+        loadInitialFragment()
+
+
+    }
+
+    private fun loadInitialFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_property_list, PropertyListFragment())
+            .commit()
+    }
+
+    private fun setupDrawerContent(navigationView: NavigationView) {
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // Handle the navigation view item clicks here.
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+    }
+
+
+    private fun setupDrawerButton() {
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            activityMainBinding.toolbar,
+            R.string.drawer_open,
+            R.string.drawer_close
+        )
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(navigationView)) {
+            drawerLayout.closeDrawer(navigationView)
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
---
+++ app/src/main/java/com/openclassrooms/realestatemanager/property_list/PropertyListFragment.kt
@@ -0,0 +1,9 @@
+package com.openclassrooms.realestatemanager.property_list
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by Julien HAMMER - Apprenti Java with openclassrooms on .
+ */
+class PropertyListFragment : Fragment() {
+}
